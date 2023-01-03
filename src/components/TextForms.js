f1edff7eb6e0e4d240d27cecbed70e636d99a2cf
Nafi <work.nafi@gmail.com>
2023-01-03 11:45:08 -0500

    counts words and characters, minutes to read, and converts upper and lowercase

--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -1,3 +1,4 @@
+
 import React, {useState} from "react";
 //{useState} is a hook
 
@@ -11,24 +12,39 @@ export default function TextForms(props) {
         setText(newText)
     }
 
+    const handleLowClick =()=>{
+      // console.log("Uppercase is clicked" + text)
+      let newText =text.toLowerCase();
+      setText(newText)
+  }
+
     const handleOnChange =(event)=>{
         // console.log("Handle Onchange")
+        
         setText(event.target.value)
     }
     
-    const [text, setText] = useState("Enter Text Here"); //text is initial variable which initially contains "Enter Text Here" this text
+    const [text, setText] = useState(""); //text is initial variable which initially contains "Enter Text Here" this text
 // and will be replaced by setText variable's values from the textarea property
 
     //text = "new text"; //wrong way to change the state
    // setText = ("new text"); //correct way to change the state
   return (
-    <div>
-    <h1>{props.heading}</h1>
-      <div className="mb-3">
-        
+    <>
+    <div className="container">
+      <h1>{props.heading}</h1>
+      <div className="mb-3">      
         <textarea className="form-control" id="myBox"  onChange={handleOnChange} value={text} rows="8"></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase  </button>
+      <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase  </button>
+      <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to lowercase</button>
     </div>
+    
+    <div className="container my-3">
+      <h1>Your Text Summary</h1>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>It will take {0.008 * text.split(" ").length} minutes to read</p>
+    </div>
+    </>
   );
 }
