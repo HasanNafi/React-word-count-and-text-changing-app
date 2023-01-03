af5862a5b615aee1f8722c8f339bebf7afe1cef5
Nafi <work.nafi@gmail.com>
2023-01-03 13:08:38 -0500

    added 3 more buttons and fixed textarea bug

--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -13,10 +13,27 @@ export default function TextForms(props) {
     }
 
     const handleLowClick =()=>{
-      // console.log("Uppercase is clicked" + text)
       let newText =text.toLowerCase();
       setText(newText)
-  }
+    }
+    const handleCapitalizeClick =()=>{
+      let newText =text.toLowerCase().split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+      // let newText =text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+      setText(newText)
+    }
+    const handleClear =()=>{
+      let newText ="";
+      setText(newText)
+    }
+    const handleEmailExtract =()=>{
+      let matchedEmails= text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi);
+      document.getElementById("email").innerHTML = matchedEmails
+      
+    }
+    const speak = () => {
+      let msg = new SpeechSynthesisUtterance(text);
+      window.speechSynthesis.speak(msg);
+    }
 
     const handleOnChange =(event)=>{
         // console.log("Handle Onchange")
@@ -37,13 +54,22 @@ export default function TextForms(props) {
         <textarea className="form-control" id="myBox"  onChange={handleOnChange} value={text} rows="8"></textarea>
       </div>
       <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase  </button>
-      <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to lowercase</button>
+      <button className="btn btn-info mx-1" onClick={handleLowClick}>Convert to Lowercase</button>
+      <button className="btn btn-success mx-1" onClick={handleCapitalizeClick}>Capitalize text</button>
+      <button className="btn btn-secondary mx-1" onClick={handleEmailExtract}>Extract Email</button>
+      <button type="submit" onClick={speak} className="btn btn-warning mx-2 my-2">Speak</button>
+      <button className="btn btn-danger mx-1" onClick={handleClear}>Clear</button>
     </div>
     
     <div className="container my-3">
       <h1>Your Text Summary</h1>
       <p>{text.split(" ").length} words and {text.length} characters</p>
       <p>It will take {0.008 * text.split(" ").length} minutes to read</p>
+    </div>
+
+    <div className="container my-3">
+      <h3>Extracted emails from text</h3>
+      <p>Emails: <span id="email"></span></p>
     </div>
     </>
   );
