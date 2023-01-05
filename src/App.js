b001ff8d54ca1db8c4e8c291e64a27d19c9fbdac
Nafi <work.nafi@gmail.com>
2023-01-05 13:25:26 -0500

    adds dark mode and 3 more buttons

--- src/App.js
+++ src/App.js
@@ -2,17 +2,33 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 
 import TextForms from "./components/TextForms";
+import About from "./components/About";
+import React,{ useState } from 'react';
 
 
 function App() {
+  const[mode,setMode] = useState('light'); //wether dark mode is on or off
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode ('dark')
+      document.body.style.backgroundColor = '#042743';
+    }
+    else{
+      setMode ('light')
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" home="Home" aboutText="About Us"/> 
+      <Navbar title="TextUtils" home="Home" aboutText="About Us" mode={mode} toggleMode={toggleMode}/> 
 
       {/* <Navbar title="Textutils"/>  */}
       {/* <Navbar/>  */}
       <div className="container my-3">
-          <TextForms heading="Enter the text to analyze"/>
+          <TextForms heading="Enter the text to analyze" mode={mode}/>
+      </div>
+      <div className="container my-3">
+        <About mode={mode}/>
       </div>
       
     </>
--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -1,5 +1,6 @@
 
 import React, {useState} from "react";
+import copy from "copy-to-clipboard";  
 //{useState} is a hook
 
 
@@ -21,10 +22,7 @@ export default function TextForms(props) {
       // let newText =text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
       setText(newText)
     }
-    const handleClear =()=>{
-      let newText ="";
-      setText(newText)
-    }
+    
     const handleEmailExtract =()=>{
       let matchedEmails= text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi);
       document.getElementById("email").innerHTML = matchedEmails
@@ -35,6 +33,24 @@ export default function TextForms(props) {
       window.speechSynthesis.speak(msg);
     }
 
+    const handleCopy = ()=>{
+      copy(text);
+      alert('Copied');
+      // var text = document.getElementById("mybox");
+      // text.Select();
+      // navigator.clipboard.writeText(text.value);
+
+    }
+
+    const handleExtraSpaces = ()=>{
+      let newText = text.split(/[ ]+/);
+      setText(newText.join(" "))
+    }
+
+    const handleClear =()=>{
+      let newText ="";
+      setText(newText)
+    }
     const handleOnChange =(event)=>{
         // console.log("Handle Onchange")
         
@@ -47,27 +63,30 @@ export default function TextForms(props) {
     //text = "new text"; //wrong way to change the state
    // setText = ("new text"); //correct way to change the state
   return (
+    
     <>
-    <div className="container">
+    <div className="container" style={{color:props.mode==='light'?'black':'white'}}>
       <h1>{props.heading}</h1>
       <div className="mb-3">      
-        <textarea className="form-control" id="myBox"  onChange={handleOnChange} value={text} rows="8"></textarea>
+        <textarea className="form-control" id="myBox" style={{backgroundColor:props.mode==='light'?'white':'grey',color:props.mode==='light'?'black':'white'}} onChange={handleOnChange} value={text} rows="8"></textarea>
       </div>
       <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase  </button>
       <button className="btn btn-info mx-1" onClick={handleLowClick}>Convert to Lowercase</button>
       <button className="btn btn-success mx-1" onClick={handleCapitalizeClick}>Capitalize text</button>
       <button className="btn btn-secondary mx-1" onClick={handleEmailExtract}>Extract Email</button>
       <button type="submit" onClick={speak} className="btn btn-warning mx-2 my-2">Speak</button>
+      <button className="btn btn-secondary mx-1" onClick={handleCopy}>Copy Text</button>
+      <button className="btn btn-info mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
       <button className="btn btn-danger mx-1" onClick={handleClear}>Clear</button>
     </div>
     
-    <div className="container my-3">
+    <div className="container my-3" style={{color:props.mode==='light'?'black':'white'}}>
       <h1>Your Text Summary</h1>
       <p>{text.split(" ").length} words and {text.length} characters</p>
       <p>It will take {0.008 * text.split(" ").length} minutes to read</p>
     </div>
 
-    <div className="container my-3">
+    <div className="container my-3" style={{color:props.mode==='light'?'black':'white'}}>
       <h3>Extracted emails from text</h3>
       <p>Emails: <span id="email"></span></p>
     </div>
